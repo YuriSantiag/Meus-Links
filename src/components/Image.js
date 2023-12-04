a35bff2cb311e3d27221cabcadc24b0c03419366
YuriSantiag <yurisantiagx5@gmail.com>
2023-12-04 10:04:39 -0300

    style: profile pic

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,7 @@
 export default function ImageComp() {
   return (
-    <div className="mt-20 w-26 border-2 rounded-full bg-white bg-opacity-30">
-      <img src="/images/prof.png" />
+    <div className="mt-20 w-24 border-2 rounded-full bg-white bg-opacity-30">
+      <img className=" rounded-full" src="/images/Profile.jpg" />
     </div>
   );
 }
